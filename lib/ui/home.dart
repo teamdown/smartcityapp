95b378ba41a33dc7dcc40b6012f3eb93ca4707c7
nyandrianina <nyandrianinamamy@gmail.com>
2018-11-24 01:01:39 +0300

    initial commit

---
+++ lib/ui/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Smart City'),
+      ),
+      body: Center(
+        child: Text('Hello from Smart City'),
+      ),
+      
+    );
+  }
+
+}

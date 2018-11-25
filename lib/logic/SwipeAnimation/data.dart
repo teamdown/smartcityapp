e40157baa461126948cd36b816f4db8c1fd906f4
Ursulla <ursullafreddylie@hotmail.fr>
2018-11-25 14:01:05 +0300

    commit event

--- lib/logic/SwipeAnimation/data.dart
+++ lib/logic/SwipeAnimation/data.dart
@@ -1,7 +1,7 @@
 import 'package:intia_app/logic/SwipeAnimation/styles.dart';
 
-List imageData = [image5, image3, image4];
-List titleData = ['Haha', 'Hoho', 'Huhu'];
-List descData = ['Description 3', 'Description 2', 'Description 1'];
-List distData = ['3', '2', '1' ];
-List timeData = ['3:00', '2:00', '1:00'];
+List imageData = [image5, image4, image3,image2,image1];
+List titleData = ['Patisserie et Vienoiserie', 'DevFest Antananarivo', 'Le SCHIVA Night-Club','Marché de Noel','Wine & Cheese & Jazz'];
+List descData = ['Atelier de formation et de partage sur le monde de la patisserie et de la vienoiserie au Stade municipale Mahamasina , entrée gratuite.', 'Un festival de developpement à l²American Center Tanjombato organisé par GDG, Bocasay, Axian et Telm ; avec le thème : Build for Digital Wellbeing in Madagascar, talks, hackathon, codelabs, réseautage, partage,...', 'Soirée de folie au Le SCHIVA Night-Club,avec Wendy CATHALINA et Bartez en live avec d²autres animations DJ , et restauration.'];
+List distData = ['5', '4', '3','2','1' ];
+List timeData = ['8:00', '9:00', '20:00', '20:00', '19:00'];
